bd65d74aad1aa18a01c702d598a2e820f20d9dc2
mixaill76 <mike.milos@yandex.ru>
2017-11-30 11:41:26 +0300

    release

--- main.java
+++ main.java
@@ -1,38 +1,66 @@
 import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
-
 class Poker {    
     
     public static void main(String[] args){
         final Random random = new Random();
-        int max = 5;
+        int max = 10;//maximum random number - 1
         
         System.out.println("Poker");
         System.out.println("Input: ");
         System.out.print("    ");
-        /*Console input*/
-        //Scanner in = new Scanner(System.in);
-        //String s = in.nextLine();
-        /*Console input*/
-        List<Integer> pd = new ArrayList<Integer>();
+
+        List<Integer> pd = new ArrayList<Integer>();//array of random numbers
         for(int i=0;i<5;i++){
             pd.add(random.nextInt(max));  
-                      
             System.out.print(pd.get(i)+" "); 
         }        
         System.out.println();
 
+        List<Integer> weight = new ArrayList<Integer>();//array of numbers weight
         for(int i=0;i<pd.size();i++){
-            int count = 1;
+            int count = 1;//number of identical numbers
             for(int w=i;w<pd.size()-1;w++){
-                if(pd.get(i) == pd.get(w+1)){
+                if(pd.get(i) == pd.get(w+1)){//if the number and the next number are equal
                     count++;  
-                    pd.remove(w);w--;                                               
+                    pd.remove(w+1);w--;//deleting duplicate numbers                                              
                 }
             }        
-           System.out.println(pd.get(i)+" - count = "+count);
+           weight.add(count);
         }
 
+        System.out.println("Output: ");
+
+        int one_count = 0;
+        for(int i=0;i<weight.size();i++){
+            if(weight.get(i) == 1)one_count++;//number of '1' in the array
+        }
+
+        String return_value = "";
+        switch(one_count)
+        {
+            case 0:
+                if(weight.size() == 2)return_value="full house";
+                else return_value="poker";
+                break;
+            case 1:
+                if(weight.size() == 3)return_value="two pairs";
+                else return_value="four of a kind";
+                break;
+            case 2:
+                return_value="three of a kind";
+                break;
+            case 3:
+                return_value="one pair";
+                break;
+            case 5:
+                return_value="all different";
+                break;
+            default :
+                System.out.println("Кол-во едениц = "+one_count);
+        }
+        System.out.println("    "+return_value);
+     
     }
 }
